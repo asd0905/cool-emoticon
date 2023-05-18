2ad117072fe082f6cca23c014e94cbdda10a61be
정용채 <asd0905@naver.com>
2023-05-18 10:34:25 +0900

    react-query-devtools 적용

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {RecoilRoot} from 'recoil';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ root.render(
             <RecoilRoot>
                 <QueryClientProvider client={queryClient}>
                     <App/>
+                    <ReactQueryDevtools initialIsOpen={false} />
                 </QueryClientProvider>
             </RecoilRoot>
         </BrowserRouter>
