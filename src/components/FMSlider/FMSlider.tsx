c106a82e0ee07d567148266e893ac645bbc0242d
asd0905 <asd0905@naver.com>
2023-05-18 23:22:06 +0900

    스타일 수정

--- src/components/FMSlider/FMSlider.tsx
+++ src/components/FMSlider/FMSlider.tsx
@@ -1,75 +1,83 @@
-import {Link} from 'react-router-dom';
-import {useEffect, useRef, useState} from 'react';
-import {useMotionValue} from 'framer-motion';
-import {EEmoticon, IEmoticon} from '../../app.constant';
-import {useRecoilState, useRecoilValue} from 'recoil';
-import {emoticonsAtom, isDraggableAtom} from '../../atoms/atom';
-import { SSlider } from './FMSlider.style';
+import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useMotionValue } from "framer-motion";
+import { EEmoticon, IEmoticon } from "../../app.constant";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { emoticonsAtom, isDraggableAtom } from "../../atoms/atom";
+import { SSlider } from "./FMSlider.style";
+import React from "react";
 
 interface ISliderType {
-    type: string;
-    list?: number[];
+	type: string;
+	list?: number[];
 }
 
-export default function FMSlider({type, list}: ISliderType) {
-    const slideRef = useRef<HTMLDivElement>(null);
-    const [constraints, setConstraints] = useState<number>(0);
-    const [isDraggable, setIsDrag] = useRecoilState<boolean>(isDraggableAtom);
-    const x = useMotionValue(0);
-    const emoticons = useRecoilValue(emoticonsAtom);
+export default function FMSlider({ type, list }: ISliderType) {
+	const slideRef = useRef<HTMLDivElement>(null);
+	const [constraints, setConstraints] = useState<number>(0);
+	const [isDraggable, setIsDrag] = useRecoilState<boolean>(isDraggableAtom);
+	const x = useMotionValue(0);
+	const emoticons = useRecoilValue(emoticonsAtom);
 
-    useEffect(() => {
-        const setIsDraggable = () => {
-            if (slideRef && slideRef.current) {
-                const clientWidth = document.body.clientWidth;
-                const constraints = slideRef.current.scrollWidth - document.body.clientWidth + 30;
-                setConstraints(constraints);
-                if (clientWidth > 1024) {
-                    setIsDrag(false);
-                } else {
-                    setIsDrag(true);
-                }
-                x.set(0);
-            }
-        }
-        setIsDraggable();
-        window.addEventListener('resize', setIsDraggable);
-        return () => window.removeEventListener('resize', setIsDraggable);
-    }, [emoticons, x])
-    return (
-        <>
-            <SSlider
-                ref={slideRef}
-                drag={isDraggable && 'x'}
-                initial={{ x: 0 }}
-                transition={{duration: 2}}
-                style={{x}}
-                dragTransition={{ bounceDamping: 30}}
-                dragConstraints={{
-                    left: -constraints,
-                    right: 0
-                }}
-                className={`${!isDraggable ? 'web' : ''}`}
-            >
-                {list && list.length > 0 ? (
-                    emoticons?.emoticonList.slice(list[0], list[1]).map((e: IEmoticon) =>
-                        <div className={'styleBannerBox'} key={e.emoticonName}>
-                            <Link to={'/'}>
-                                <img src={`${emoticons?.baseUrl}${e.emoticonName}/image_pack/${e.images[0].image}`} alt="img" />
-                            </Link>
-                        </div>
-                    )
-                ) : (
-                    emoticons?.emoticonList.map((e: IEmoticon) =>
-                        <div className={`newBannerBox ${type}`} key={e.emoticonName}>
-                            <Link to={'/'}>
-                                <img src={`${emoticons?.baseUrl}${e.emoticonName}/image_pack/${e.images[0].image}`} alt="img" />
-                                <p>{e.emoticonName}</p>
-                            </Link>
-                        </div>
-                    )
-                )}
-            </SSlider>
-        </>
-    )
+	useEffect(() => {
+		const setIsDraggable = () => {
+			if (slideRef && slideRef.current) {
+				const clientWidth = document.body.clientWidth;
+				const constraints =
+					slideRef.current.scrollWidth - document.body.clientWidth + 30;
+				setConstraints(constraints);
+				if (clientWidth > 1024) {
+					setIsDrag(false);
+				} else {
+					setIsDrag(true);
+				}
+				x.set(0);
+			}
+		};
+		setIsDraggable();
+		window.addEventListener("resize", setIsDraggable);
+		return () => window.removeEventListener("resize", setIsDraggable);
+	}, [emoticons, x]);
+	return (
+		<>
+			<SSlider
+				ref={slideRef}
+				drag={isDraggable && "x"}
+				initial={{ x: 0 }}
+				transition={{ duration: 2 }}
+				style={{ x }}
+				dragTransition={{ bounceDamping: 30 }}
+				dragConstraints={{
+					left: -constraints,
+					right: 0,
+				}}
+				className={`${!isDraggable ? "web" : ""}`}
+			>
+				{list && list.length > 0
+					? emoticons?.emoticonList
+							.slice(list[0], list[1])
+							.map((e: IEmoticon) => (
+								<div className={"styleBannerBox"} key={e.emoticonName}>
+									<Link to={"/"}>
+										<img
+											src={`${emoticons?.baseUrl}${e.emoticonName}/image_pack/${e.images[0].image}`}
+											alt='img'
+										/>
+									</Link>
+								</div>
+							))
+					: emoticons?.emoticonList.map((e: IEmoticon) => (
+							<div className={`newBannerBox ${type}`} key={e.emoticonName}>
+								<Link to={"/"}>
+									<img
+										src={`${emoticons?.baseUrl}${e.emoticonName}/image_pack/${e.images[0].image}`}
+										alt='img'
+									/>
+									<p>{e.emoticonName}</p>
+								</Link>
+							</div>
+					  ))}
+			</SSlider>
+		</>
+	);
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,64 +1,84 @@
-import {useQuery} from 'react-query';
-import {getEmoticon} from '../../api';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import {EEmoticon, EQueryKey, IEmoticonData} from '../../app.constant';
-import {useRecoilValue, useSetRecoilState} from 'recoil';
-import {emoticonsAtom, isDraggableAtom} from '../../atoms/atom';
-import FMSlider from '../../components/FMSlider/FMSlider';
-import { SLayout, STitle3 } from './Main.style';
-import MainBanner from '../../components/MainBanner/MainBanner';
+import { useQuery } from "react-query";
+import { getEmoticon } from "../../api";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { EEmoticon, EQueryKey, IEmoticonData } from "../../app.constant";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { emoticonsAtom, isDraggableAtom } from "../../atoms/atom";
+import FMSlider from "../../components/FMSlider/FMSlider";
+import { SLayout, STitle3 } from "./Main.style";
+import MainBanner from "../../components/MainBanner/MainBanner";
+import React from "react";
 
 export default function Main() {
-    const setEmoticons = useSetRecoilState<IEmoticonData>(emoticonsAtom);
-    const isDraggable = useRecoilValue<boolean>(isDraggableAtom);
-    const { isLoading} = useQuery([EQueryKey.EMOTICONS], getEmoticon,{
-        // refetchOnWindowFocus: false,
-        onSuccess: (response) => {
-            setEmoticons(response.data)
-        }
-    });
-    // console.log(isLoading, data)
-    return <>
-        <SLayout>
-            <MainBanner type={'mainBanner'}>
-                <h1>쿨서비스 이모티콘</h1>
-                <video autoPlay={true} muted={true} loop={true} playsInline={true} >
-                    <source src="//update.coolmessenger.com/_ImageServer/jirancomms/img/상단.mp4" type="video/mp4" />
-                </video>
-            </MainBanner>
-            {isLoading ? <div>Loading...</div> :
-                <>
-                    <article className={'newBannerWrap'}>
-                        <h2 className={'bannerTitle'}>
-                            <Link to={'/'}>
-                                신규 이모티콘
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                    <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-                                </svg>
-                            </Link>
-                        </h2>
-                        <FMSlider type={EEmoticon.NEW} />
-                    </article>
-                    <section className={'styleBannerWrap'}>
-                        <h2 className={'bannerTitle'}>
-                            <Link to={'/'}>
-                                스타일
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                    <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-                                </svg>
-                            </Link>
-                        </h2>
-                        <article className={!isDraggable ? 'web' : ''}>
-                            <STitle3 color={'rgb(244, 196, 0)'}><strong>#귀여운</strong><span>#선생님</span></STitle3>
-                            <FMSlider type={EEmoticon.STYLE} list={[6, 10]} />
-                        </article>
-                        <article className={!isDraggable ? 'web' : ''}>
-                            <STitle3 color={'rgb(144, 199, 100)'}><strong>#동물</strong><span>#쿨 캐릭터</span></STitle3>
-                            <FMSlider type={EEmoticon.STYLE} list={[2, 6]} />
-                        </article>
-                    </section></>
-            }
-        </SLayout>
-    </>
+	const setEmoticons = useSetRecoilState<IEmoticonData>(emoticonsAtom);
+	const isDraggable = useRecoilValue<boolean>(isDraggableAtom);
+	const { isLoading } = useQuery([EQueryKey.EMOTICONS], getEmoticon, {
+		// refetchOnWindowFocus: false,
+		onSuccess: (response) => {
+			setEmoticons(response.data);
+		},
+	});
+	// console.log(isLoading, data)
+	return (
+		<>
+			<SLayout>
+				<MainBanner type={"mainBanner"}>
+					<h1>쿨서비스 이모티콘</h1>
+					<video autoPlay={true} muted={true} loop={true} playsInline={true}>
+						<source
+							src='//update.coolmessenger.com/_ImageServer/jirancomms/img/상단.mp4'
+							type='video/mp4'
+						/>
+					</video>
+				</MainBanner>
+				{isLoading ? (
+					<div>Loading...</div>
+				) : (
+					<>
+						<article className={"newBannerWrap"}>
+							<h2 className={"bannerTitle"}>
+								<Link to={"/"}>
+									신규 이모티콘
+									<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+										<path d='M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z' />
+									</svg>
+								</Link>
+							</h2>
+							<FMSlider type={EEmoticon.NEW} />
+						</article>
+						<div className='styleBannerWrapDiv'>
+							<section className={"styleBannerWrap"}>
+								<h2 className={"bannerTitle"}>
+									<Link to={"/"}>
+										스타일
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											viewBox='0 0 448 512'
+										>
+											<path d='M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z' />
+										</svg>
+									</Link>
+								</h2>
+								<article className={!isDraggable ? "web" : ""}>
+									<STitle3 color={"rgb(244, 196, 0)"}>
+										<strong>#귀여운</strong>
+										<span>#선생님</span>
+									</STitle3>
+									<FMSlider type={EEmoticon.STYLE} list={[6, 11]} />
+								</article>
+								<article className={!isDraggable ? "web" : ""}>
+									<STitle3 color={"rgb(144, 199, 100)"}>
+										<strong>#동물</strong>
+										<span>#쿨 캐릭터</span>
+									</STitle3>
+									<FMSlider type={EEmoticon.STYLE} list={[1, 6]} />
+								</article>
+							</section>
+						</div>
+					</>
+				)}
+			</SLayout>
+		</>
+	);
 }
